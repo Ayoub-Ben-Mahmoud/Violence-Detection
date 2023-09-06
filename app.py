dc1fb9708efbfbbab9a0f2e14d430763921266f8
Ayoub Ben Mahmoud <ayoubb917@gmail.com>
2023-09-06 23:23:32 +0100

    the web part almost done

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ import numpy as np
 import os
 from keras.models import load_model
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 
 IMAGE_HEIGHT = 64
 IMAGE_WIDTH = 64
-SEQUENCE_LENGTH = 10
+SEQUENCE_LENGTH = 15
 CLASSES_LIST = ["NonViolence", "Violence"]
 
 model = load_model("C:\D\Study\Internship\AMZI Smart Solutions\Project\Model.h5")
@@ -18,6 +18,8 @@ def preprocess_video(video_path):
 
     # Open the video file
     video_capture = cv2.VideoCapture(video_path)
+
+    frame_count = 0  # Initialize a frame count variable
 
     while True:
         ret, frame = video_capture.read()
@@ -33,14 +35,26 @@ def preprocess_video(video_path):
 
         frames_list.append(frame)
 
+        frame_count += 1  # Increment frame count
+
+        # Check if we have reached the desired number of frames (SEQUENCE_LENGTH)
+        if frame_count == SEQUENCE_LENGTH:
+            break  # Exit the loop when we have enough frames
+
     video_capture.release()
 
     # Ensure that the number of frames matches SEQUENCE_LENGTH
-    if len(frames_list) != SEQUENCE_LENGTH:
-        # Handle this case as needed, e.g., by duplicating frames or skipping the video
-        pass
+    while len(frames_list) < SEQUENCE_LENGTH:
+        # If we have fewer frames, duplicate the last frame to match the desired length
+        frames_list.append(frames_list[-1].copy())
 
-    return frames_list
+    # Convert the list of frames to a numpy array with shape (SEQUENCE_LENGTH, IMAGE_HEIGHT, IMAGE_WIDTH, 3)
+    frames_array = np.array(frames_list)
+
+    # Expand the dimensions to match the expected input shape (None, SEQUENCE_LENGTH, IMAGE_HEIGHT, IMAGE_WIDTH, 3)
+    frames_array = np.expand_dims(frames_array, axis=0)
+
+    return frames_array
 
 def predict_video(video_path):
     frames_list = preprocess_video(video_path)
@@ -52,7 +66,12 @@ def predict_video(video_path):
 
     final_prediction = combine_predictions(predictions)
 
-    return final_prediction
+    # Get the predicted class and confidence
+    predicted_class_idx = np.argmax(final_prediction)
+    predicted_class = CLASSES_LIST[predicted_class_idx]
+    confidence = final_prediction[0][predicted_class_idx]
+
+    return predicted_class, confidence
 
 def combine_predictions(predictions):
     # Combine predictions if necessary (e.g., averaging)
@@ -66,14 +85,22 @@ def index():
 @app.route('/predict', methods=['POST'])
 def predict():
     try:
+        if 'video' not in request.files:
+            return render_template('index.html', error="No video selected for upload")
+
         video_file = request.files['video']
+        
+        # Check if the file is empty
+        if video_file.filename == '':
+            return render_template('index.html', error="No video selected for upload")
+
         video_path = 'temp_video.mp4'
         video_file.save(video_path)
-        prediction = predict_video(video_path)
-        class_name = CLASSES_LIST[np.argmax(prediction)]
-        return render_template('index.html', prediction=class_name)
+        predicted_class, confidence = predict_video(video_path)
+        return render_template('index.html', prediction=predicted_class, confidence=confidence)
     except Exception as e:
-        return jsonify({'error': str(e)})
+        return render_template('index.html', error=str(e))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- setup.py
+++ setup.py
@@ -1,19 +1,27 @@
-from setuptools import find_packages,setup
+from setuptools import find_packages, setup
 from typing import List
 
-def get_requirements(file_path:str)->List[str]:
+HYPEN_E_DOT = '-e .'
+
+def get_requirements(file_path: str) -> List[str]:
     '''
     this function will return the list of requirements
     '''
     requirements = []
-    with open(file_path,'r') as f:
-        requirements = f.read().splitlines()
+    with open(file_path) as f:
+        requirements = f.readlines()
+        requirements = [req.replace("\n", "") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:  # Use the variable HYPEN_E_DOT, not "HYPEN_E_DOT"
+            requirements.remove(HYPEN_E_DOT)
+
+    return requirements
 
 setup(
-name='InernProject',
-version='0.0.1',
-author='Ayoub Ben Mahmoud',
-author_email='ayoubb917@gmail.com',
-packages=find_packages(),   
-install_requires=get_requirements('requirements.txt')
+    name='InternProject',
+    version='0.0.1',
+    author='Ayoub Ben Mahmoud',
+    author_email='ayoubb917@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
 )
